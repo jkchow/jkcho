a739d9ff065d67f92b34532424c3b0deb0c98fa6
jkcho <jkzzit@163.com>
2018-03-04 00:47:08 +0800

    12

--- src/hashmap/Test.java
+++ src/hashmap/Test.java
@@ -2,6 +2,10 @@ package hashmap;
 
 public class Test {
 	public static void main(String[] args) {
-
+		int sum=0;
+		for (int i = 1; i <=10000; i++) {
+			sum=sum+i;
+		}
+		System.out.println("1+2+3+.....+9999+10000的结果是"+sum);
 	}
 }
